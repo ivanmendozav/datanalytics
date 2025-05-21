07c26657259498066ba58436c14d20a241626670
ivanmendozav <ivanmendozav@yahoo.com>
2025-05-21 10:30:15 -0500

    Barras con plotly

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Mi segunda publicacion")
 st.header("Introduccion")
 st.write("Esta es la primera que me sale algo.")
+
+fig = px.bar(x=["A","B","c"],
+             y=[4,8,6])
+st.plotly_chart(fig)
